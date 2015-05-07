13702605c9282af15a0bb48604ea9f4700ac6a4f
rapucha <rapucha@gmail.com>
2015-05-08 02:21:20 +0300

    some refactoring Timer task

--- src/com/toad/Main.java
+++ src/com/toad/Main.java
@@ -1,9 +1,16 @@
 package com.toad;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.json.*;
@@ -11,17 +18,44 @@ import org.json.*;
 /*    /\[([^\]]+)]/ */
 class Main {
 
-    public static final String PATTERN = "\\[([^\\]]+)];";
 
     public static void main(String[] args) throws Exception {
-        Pattern pattern = Pattern.compile("var stationsData = "+ PATTERN);
+        Crawler.timer.scheduleAtFixedRate(new CrawlerTask(), 500, 600); //delay in milliseconds
+        while(true) {
+            Thread.sleep(100);
+        }
+    }
 
+}
+ class Crawler{
+     static Timer timer = new Timer();
+     static final String PATTERN = "\\[([^\\]]+)];";
+     public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+
+
+    static void crawl() throws IOException {
+        Pattern pattern = Pattern.compile("var stationsData = "+ PATTERN);
         System.setProperty("java.net.useSystemProxies", "true");
-        URL velo = new URL("http://spb.velogorod.org/");
-        URLConnection uc = velo.openConnection();
-        uc.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
-        BufferedReader in = new BufferedReader(new InputStreamReader(
-                uc.getInputStream()));
+        URL velo = null;
+        try {
+            velo = new URL("http://spb.velogorod.org/");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        URLConnection uc = null;
+        try {
+            uc = velo.openConnection();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        uc.setRequestProperty("User-Agent", USER_AGENT);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(new InputStreamReader(
+                    uc.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         String inputLine;
         int total=0;
         while ((inputLine = in.readLine()) != null){
@@ -40,6 +74,18 @@ class Main {
         }
         System.out.println(total);
         in.close();
+
+    }
+
+ }
+class CrawlerTask extends TimerTask {
+
+    @Override
+    public void run() {
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+        //Crawler.timer.cancel();
 
     }
 }
